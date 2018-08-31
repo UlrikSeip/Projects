8fd58b63dcd1059ee63ea600f550731a91aa8bb6
Ulrik Seip <ulrik.seip@gmail.com>
2018-08-31 15:19:55 +0200

    working on that automization

--- prosjekt1/test_b.py
+++ prosjekt1/test_b.py
@@ -6,8 +6,33 @@ Created on Mon Aug 27 17:05:33 2018
 """
 
 from numpy import zeros, exp, linspace
+import matplotlib.pyplot as plt
 
-n = 4
+def f(x):
+    return (100*exp(-10*x))
+
+def calc(x, h):
+    for i in range(n-1):
+        a[i] = -1; c[i] = -1
+    for i in range(n):
+        b[i] = 2
+        B[i] = h**2*f(x[i])
+
+        
+    b_[0] = b[0]
+    B_[0] = B[0]
+    for i in range(1, n):
+        b_[i] = b[i] - a[i-1]*c[i-1]/b_[i-1]
+        B_[i] = B[i] - a[i-1]*B_[i-1]/b_[i-1]
+
+    k[n-1] = B_[n-1]
+    i = n-2
+    while i >= 0:
+        k[i] = B_[i] - c[i]*k[i+1]/b_[i]
+        i-=1
+    return()
+
+n = int(1e7)
 a = zeros(n-1)
 b = zeros(n)
 c = zeros(n-1)
@@ -21,30 +46,10 @@ x = linspace(0, 1, n)
 
 h = 1/(n+1)
 
-def f(x):
-    return (100*exp(-10*x))
-
-for i in range(n-1):
-    a[i] = -1; c[i] = -1
-for i in range(n):
-    b[i] = 2
-    B[i] = h**2*f(x[i])
-
-    
-b_[0] = b[0]
-B_[0] = B[0]
-for i in range(1, n):
-    b_[i] = b[i] - a[i-1]*c[i-1]/b_[i-1]
-    B_[i] = B[i] - a[i-1]*B_[i-1]/b_[i-1]
-
-k[n-1] = B_[n-1]
-i = n-2
-while i >= 0:
-    k[i] = B_[i] - c[i]*k[i+1]/b_[i]
-    i-=1
-    
 print(k)
 print(b_)
 
 print(k/b_)
 
+plt.plot(x, k)
+plt.show()
