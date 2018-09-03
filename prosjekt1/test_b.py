c3977d152c6f6f8b89ddea959dd5a42cd2d12f9b
Cosmaus <ulriksspamepost@gmail.com>
2018-09-03 19:50:29 +0200

    merging

--- prosjekt1/test_b.py
+++ prosjekt1/test_b.py
@@ -1,55 +1,65 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 27 17:05:33 2018
+import seaborn
+from numpy import zeros, exp, linspace, ones
+from matplotlib.pyplot import plot, show, title, xlabel, ylabel, legend, savefig
 
-@author: Bendik
-"""
-
-from numpy import zeros, exp, linspace
-import matplotlib.pyplot as plt
-
-def f(x):
-    return (100*exp(-10*x))
-
-def calc(x, h):
-    for i in range(n-1):
-        a[i] = -1; c[i] = -1
+def rref(n):
+    a = ones(n-1)
+    a *= -1
+    b = ones(n)
+    b *= 2
+    c = ones(n-1)
+    c *= -1
+    
+    B = zeros(n)
+    #v = zeros(n)
+    b_ = zeros(n)
+    k = zeros(n)
+    B_ = zeros(n)
+    x = linspace(0, 1, n)
+    
+    h = 1/(n+1)
+    
+    def f(x):
+        return (100*exp(-10*x))
+    
+    u = 1 - (1 - exp(-10))*x - exp(-10*x)
+    
     for i in range(n):
-        b[i] = 2
         B[i] = h**2*f(x[i])
-
+    
         
     b_[0] = b[0]
     B_[0] = B[0]
     for i in range(1, n):
-        b_[i] = b[i] - a[i-1]*c[i-1]/b_[i-1]
-        B_[i] = B[i] - a[i-1]*B_[i-1]/b_[i-1]
-
+        temp = a[i-1]/b_[i-1]
+        b_[i] = b[i] - temp*c[i-1]
+        B_[i] = B[i] - temp*B_[i-1]
+    
     k[n-1] = B_[n-1]
     i = n-2
     while i >= 0:
         k[i] = B_[i] - c[i]*k[i+1]/b_[i]
         i-=1
-    return()
+    return x, k, b_, u
 
-n = int(1e7)
-a = zeros(n-1)
-b = zeros(n)
-c = zeros(n-1)
+def plot_rref(n):
+    x, k, b_, u = rref(n) 
+    
+    plot(x, k)
+    leg.append("n: " + str(n))
+    return x, u
 
-B = zeros(n)
-v = zeros(n)
-b_ = zeros(n)
-k = zeros(n)
-B_ = zeros(n)
-x = linspace(0, 1, n)
+leg = []
+plot_rref(10)
+plot_rref(100)
+x, u = plot_rref(1000)
+plot_rref(10000)
 
-h = 1/(n+1)
 
-print(k)
-print(b_)
-
-print(k/b_)
-
-plt.plot(x, k)
-plt.show()
+plot(x, u)
+xlabel("x"); ylabel("f(x)")
+leg.append("exact")
+legend(leg)
+title("Plot of the aprox. and exact solutions")
+savefig("test_b.pdf")
+show()
