0ab6caf5eb29b236f3eeba7e58bb709b4395b4e6
bendkok <bendiksd@student.matnat.uio.no>
2018-09-03 15:02:42 +0200

    Tror jeg klarte å fikse problemet

--- prosjekt1/test_b.jl
+++ prosjekt1/test_b.jl
@@ -8,35 +8,43 @@ function rref(n)
     a = fill(-1, n-1) #linjen under diagonalen
     b = fill(2, n) #diagonalen
     c = fill(-1, n-1) #linjen over diagonalen
+    c -= a
 
     B = zeros(n) #høyre side av likningen
     b_ = zeros(n) #den reduserte b
     B_ = zeros(n) #den reduserte B
     k = zeros(n) #det endelige resultatet
     x = range(0, stop=1, length=n) #x-verdiene
-    #u = 1.0 - (1.0 - exp(-10.0))*x - exp(-10.0*x) #den eksakte løsningen, fungerer ikke av en eller annen grunn
-
-    h = 1/(n+1)
+    
+    l = zeros(n)
     for i in range(1, length=n, step=1)
+        l[i] = exp(-10.0*x[i])
+    end
+    u = fill(1.0, n) - (1.0 - exp(-10.0))*x - l #den eksakte løsningen
+
+    h = 1/(n+1) #steglengde
+    for i in range(1, length=n, step=1) # setter inn verdiene til B
         B[i] = h^2*f(x[i])
     end 
 
     b_[1] = b[1]
     B_[1] = B[1]
 
-    for i in range(2, stop=n, step=1)
+    for i in range(2, stop=n, step=1) #forward substitution
         temp = a[i-1]/b_[i-1]
         b_[i] = b[i] - temp*c[i-1]
         B_[i] = B[i] - temp*B_[i-1]
     end
 
     k[n] = B_[n]
-    for i in range(n-1, stop=1, step=-1)
+    for i in range(n-1, stop=1, step=-1) #backward substitution
         k[i] = B_[i] - c[i]*k[i+1]/b_[i]
     end
-    return x, k, b_#, u
+    return x, k, b_, u
 
 end
 
 rref(3)
-println(2)
+
+
+
--- prosjekt1/test_b.py
+++ prosjekt1/test_b.py
@@ -7,7 +7,7 @@ Created on Mon Aug 27 17:05:33 2018
 
 import seaborn
 from numpy import zeros, exp, linspace, ones
-from matplotlib.pyplot import plot, show, title, xlabel, ylabel, legend
+from matplotlib.pyplot import plot, show, title, xlabel, ylabel, legend, savefig
 
 def rref(n):
     a = ones(n-1)
@@ -53,16 +53,26 @@ def plot_rref(n):
     x, k, b_, u = rref(n) 
     
     plot(x, k)
-    plot(x, u)
-    xlabel("x"); ylabel("f(x)")
-    legend(["Aprox.", "Exact"])
-    title("Plot of the aprox. and exact solution with " + str(n) + " grid points")
-    show()
-    
+    leg.append("n: " + str(n))
+    return x, u
+
+leg = []
 plot_rref(10)
 plot_rref(100)
-plot_rref(1000)
+x, u = plot_rref(1000)
 plot_rref(10000)
+
+
+plot(x, u)
+xlabel("x"); ylabel("f(x)")
+leg.append("exact")
+legend(leg)
+title("Plot of the aprox. and exact solutions")
+savefig("test_b.pdf")
+show()
+
+
+
 
 
 
