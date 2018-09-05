00bd5e09c6a9a2884392d5541a75e25c3c79b5a1
bendkok <bendiksd@student.matnat.uio.no>
2018-09-05 12:23:38 +0200

    Fjernet now

--- prosjekt1/tid.jl
+++ prosjekt1/tid.jl
@@ -4,18 +4,10 @@ include("test_c.jl")
 
 N = [10, Int64(1e2), Int64(1e3), Int64(1e4), Int64(1e5), Int64(1e6), Int64(1e7)]
 for n in N
-    b_start = now()
     print("b med " * string(n) * " n: ")
     @time rref_b(n)
-    b_stopp = now()
 
-    c_start = now()
     print("c med " * string(n) * " n: ")
     @time rref_c(n)
-    c_stopp = now()
 
-    b_tid =  Microsecond(b_stopp - b_start)
-    c_tid =  Microsecond(c_stopp - c_start)
-
-    #println("For n=" * string(n) * " bruker b-funksjonen " * string(b_tid) * " og c-funksjonen " * string(c_tid) * ".")
 end
