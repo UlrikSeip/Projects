55fc3b756858d3e5bfccea9e2e3288d3df873c96
bendkok <bendiksd@student.matnat.uio.no>
2018-10-16 20:31:56 +0200

    Noen optimaliseringer
    Hvis dutar acos(cos(x)) får du x, så endret på det. Trenger ikke å definere rip to ganger.
    
    En docstring skal ha """ """, ikke #. Du bør nok og legge til hva funksjonen gjør.

--- prosjekt3/integrator.jl
+++ prosjekt3/integrator.jl
@@ -2,7 +2,8 @@ using PyCall
 using PyPlot
 @pyimport matplotlib.pyplot as plotter
 @pyimport numpy as np
-import LinearAlgebra: norm
+#import LinearAlgebra: norm
+using LinearAlgebra
 
 #by starting a julia shell, and typing "]" you cna then "add PyCall"
 #and "add PyPlot" to install packages
@@ -28,10 +29,12 @@ end
 
 
 function forward_euler(vel0, pos0, t, dt, endvalue)
-    #NB!! simplified for circular motion in G-field
-    #vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
-    #endvalues is a bool, return only resulting value after time t
-    #otherwise return entire pos and vel array
+"""
+NB!! simplified for circular motion in G-field
+vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
+endvalues is a bool, return only resulting value after time t
+otherwise return entire pos and vel array
+"""
     
     #creating initial values
     length = int(t/dt)
@@ -42,8 +45,8 @@ function forward_euler(vel0, pos0, t, dt, endvalue)
     #integration loop
     for i = 2:length
         rip = norm(pos[:, i-1]) #new r
-        th = acos(pos[:, i-1]/rip) #new angle
-        v[:, i] = vel[:, i-1]-4*pi*dt/(rip^2)*cos(th) #new vel
+        th = pos[:, i-1]/rip #new angle
+        v[:, i] = vel[:, i-1]-4*pi*dt/(rip^2)*th #new vel
         pos[:, i] = pos[:, i-1] + dt*v[:, i] #new pos
     end
     #return related stuff
@@ -54,9 +57,11 @@ function forward_euler(vel0, pos0, t, dt, endvalue)
 end
 
 function velocity_verlet(vel0, pos0, t, dt, endvalue)
-    #vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
-    #endvalues is a bool, return only resulting value after time t
-    #otherwise return entire pos and vel array
+"""
+vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
+endvalues is a bool, return only resulting value after time t
+otherwise return entire pos and vel array
+"""
 
     #initial values
     length = Int64(round(t/dt))
@@ -64,15 +69,16 @@ function velocity_verlet(vel0, pos0, t, dt, endvalue)
     vel[:, 1] = vel0
     pos = ones(3, length)
     pos[:, 1] = pos0
+    rip = norm(pos[:, 1])
 
     #integration loop
     for i = 2:length
-        rip = norm(pos[:, i-1]) #current radius
-        th = acos(pos[:, i-1]/rip) #current angle
-        aip = -4*pi^2/(rip^2)*cos(th)   #current acceleration
+        #rip = norm(pos[:, i-1]) #current radius
+        th = pos[:, i-1]/rip #current angle
+        aip = -4*pi^2/(rip^2)*th   #current acceleration
         pos[:, i] = pos[:, i-1] + dt*vel[:, i-1] + ((dt^2)/2)*aip   #new position
-        ri = rip = norm(pos[:, i])  #new radius
-        ai = -4*pi^2/(ri^2)*cos(th) #new acceleration based on new radius
+        rip = norm(pos[:, i])  #new radius
+        ai = -4*pi^2/(rip^2)*th #new acceleration based on new radius
         vel[:, i] = vel[:, i-1] + dt*(ai + aip)/2   #new velocity based on new acceleration
     end
     #return related stuff
