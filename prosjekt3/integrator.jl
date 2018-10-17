21a61de2976ab3ca49cb7c6506943c66e981f6e3
Cosmaus <ulriksspamepost@gmail.com>
2018-10-17 15:36:36 +0200

    fungerer marginalt bedre

--- prosjekt3/integrator.jl
+++ prosjekt3/integrator.jl
@@ -1,9 +1,11 @@
 using PyCall
 using PyPlot
-@pyimport matplotlib.pyplot as pl
-@pyimport numpy as np
-#import LinearAlgebra: norm
 using LinearAlgebra
+using DelimitedFiles
+#using Math
+@pyimport matplotlib.pyplot as plotter
+@pyimport numpy as np
+import LinearAlgebra: norm
 
 #by starting a julia shell, and typing "]" you cna then "add PyCall"
 #and "add PyPlot" to install packages
@@ -29,12 +31,10 @@ end
 
 
 function forward_euler(vel0, pos0, t, dt, endvalue)
-"""
-NB!! simplified for circular motion in G-field
-vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
-endvalues is a bool, return only resulting value after time t
-otherwise return entire pos and vel array
-"""
+    #NB!! simplified for circular motion in G-field
+    #vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
+    #endvalues is a bool, return only resulting value after time t
+    #otherwise return entire pos and vel array
     
     #creating initial values
     length = int(t/dt)
@@ -45,23 +45,22 @@ otherwise return entire pos and vel array
     #integration loop
     for i = 2:length
         rip = norm(pos[:, i-1]) #new r
-        th = pos[:, i-1]/rip #new angle
-        v[:, i] = vel[:, i-1]-4*pi*dt/(rip^2)*th #new vel
+        print(pos[:, i-1])
+        th = acos(pos[:, i-1]/rip) #new angle
+        v[:, i] = vel[:, i-1]-4*pi*dt/(rip^2)*cos(th) #new vel
         pos[:, i] = pos[:, i-1] + dt*v[:, i] #new pos
     end
     #return related stuff
     if endvalue
-        return pos[:, end], vel[:, end]
+        return pos[:, -1], vel[:, -1]
     end
     return pos, vel
 end
 
-function velocity_verlet(vel0, pos0, t, dt, endvalue)
-"""
-vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
-endvalues is a bool, return only resulting value after time t
-otherwise return entire pos and vel array
-"""
+function velocity_verlet(vel0, pos0, t, dt, endvalue = false)
+    #vel0 and pos0 should be (x, y, z) arrays with initial values for vel an pos
+    #endvalues is a bool, return only resulting value after time t
+    #otherwise return entire pos and vel array
 
     #initial values
     length = Int64(round(t/dt))
@@ -69,16 +68,21 @@ otherwise return entire pos and vel array
     vel[:, 1] = vel0
     pos = ones(3, length)
     pos[:, 1] = pos0
-    rip = norm(pos[:, 1])
 
     #integration loop
     for i = 2:length
-        #rip = norm(pos[:, i-1]) #current radius
-        th = pos[:, i-1]/rip #current angle
-        aip = -4*pi^2/(rip^2)*th   #current acceleration
+        rip = norm(pos[:, i-1]) #current radius
+        th = acos.(pos[:, i-1]/rip) #current angle
+
+        aip = (-4*pi^2)/(rip^2)*cos.(th)   #current acceleration
         pos[:, i] = pos[:, i-1] + dt*vel[:, i-1] + ((dt^2)/2)*aip   #new position
-        rip = norm(pos[:, i])  #new radius
-        ai = -4*pi^2/(rip^2)*th #new acceleration based on new radius
+        if i == 2
+            println(th)
+            println(aip)
+            println(pos[:, i])
+        end
+        ri = rip = norm(pos[:, i])  #new radius
+        ai = -4*pi^2/(ri^2)*cos.(th) #new acceleration based on new radius
         vel[:, i] = vel[:, i-1] + dt*(ai + aip)/2   #new velocity based on new acceleration
     end
     #return related stuff
@@ -88,13 +92,35 @@ otherwise return entire pos and vel array
     return pos, vel
 end
 
+function filewriter(dataArray, filename = "orbits.txt")
+    #dataArray should be a 3d array, and is written to filename as a[:, 1]\n, a[:, 2]\n...
+    f = open(filename,"w")
+    for i = 1:Int64(length(dataArray)/3)
+        writedlm(f, dataArray[:, i])
+    end
+    close(f)
+end
+
+#i think we need a function for acceleration. our orbits mainly have positive values...
+
+"""
+function aFunk(r, ang, mass = 1)
+G = 0.01720209895
+return G*mass/(r^2)*cos.(ang)
+end
+"""
+
+
 
 v0 = [2.24e-03, 1.51e-02, 2.61e-0]
 x0 = [9.41e-01, 3.38e-01, -9.33e-05]
-stopTime = 3.154e+7
-pos, vel = velocity_verlet(v0, x0, stopTime, 3600, false)
-#println(pos[1,:])
-
+stopTime = 3
+#3.154e+7
+pos, vel = velocity_verlet(v0, x0, stopTime, stopTime/36000)
+filewriter(pos)
 #read info from file
-pl.plot(np.linspace(0, stopTime, 8761), pos[1,:])
-pl.show()
+
+println(size(pos))
+#println(pos)
+plotter.plot(pos[1], np.linspace(0, stopTime, length(pos[1])))
+plotter.show()
--- prosjekt3/solarsystem.py
+++ prosjekt3/solarsystem.py
@@ -1,24 +1,32 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class solsys():
 
     def __init__(self):
-        self.planets = []
-        self.planPos = np.array()
-        self.planVel = np.array()
         self.time = 0
 
-    def values(self, valueFile):
-        valfil = open(valieFile, "r")
-        #do reading stuff
-        #assign values to initiated variables
+    def importValues(self, filename):
+        with open(filename, "r") as infile:
+            data = infile.readlines()
+        counter = 0
+        self.planPos = np.zeros((3, int(len(data)/3)))
+        for i in range(len(data)):
+            if counter == 3:
+                counter = 0
+            if counter == 0:
+                self.planPos[0, int(np.ceil(i/3)-1)] = float(data[i])
+            if counter == 1:
+                self.planPos[1, int(np.ceil(i/3)-1)] = float(data[i])
+            if counter == 2:
+                self.planPos[2, int(np.ceil(i/3)-1)] = float(data[i])
+            counter += 1
 
-    def forward(self, intFunk, howFar, timeUnit): #runs simulation "howFar" "timeUnit"s forwards
-        #something about converting time to seconds
-        #case: seconds, hours, days, years
-        #integrating "howFar" forwards using "intFunk"
-            #sending start info to file
-            #reading and running in julia
-            #writing to file in julia
-            #reading back
-        #return/plot result
+    def plottXYOrbit(self):
+        plt.plot(np.linspace(0, 1, len(self.planPos[1])),self.planPos[1])
+        plt.show()
+
+solarsystem = solsys()
+solarsystem.importValues("orbits.txt")
+solarsystem.plottXYOrbit()
+print(solarsystem.planPos[2, -4])
