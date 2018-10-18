b6bd57a8bbf7b778c3af29c1c96d77422a3216bf
Cosmaus <ulriksspamepost@gmail.com>
2018-10-18 22:50:44 +0200

    Lagde en To-Do liste

---
+++ prosjekt3/toDoList.py
@@ -0,0 +1,8 @@
+"""
+Lagre de integrerte verdiene på en bedre måte (renere format)
+Integrere over planetobjekter i stedet for vektorer
+Lage støtte for mer enn én planet
+Plotte alle planetene i 3d
+Integrere rundt et massesenter med hensyn på alle massene, og ikke bare sola
+Skrive rapport
+"""
