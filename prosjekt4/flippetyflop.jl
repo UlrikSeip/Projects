169c6384f48e675e2d817c64aec18af87a12f833
Cosmaus <ulriksspamepost@gmail.com>
2018-11-21 23:56:27 +0100

    program done

--- prosjekt4/flippetyflop.jl
+++ prosjekt4/flippetyflop.jl
@@ -6,6 +6,7 @@ const plt = PyPlot
 println("Importing small stuff")
 @time using Random
 @time using Printf
+@time using Dates
 @time import LinearAlgebra: norm
 @time import Statistics
 @time import Base.Threads
@@ -14,7 +15,6 @@ const kb = 8.6173303E-5
 
 println()
 
-const plt = PyPlot
 const threads = Sys.CPU_THREADS
 Threads.nthreads() = threads
 
@@ -33,8 +33,8 @@ function get_E(M, J = 1)
 
     L = length(M[1, :])
     E_tot = 0
-    for i = 1:2:L
-        for j = 1:2:L
+    @inbounds for i = 1:2:L
+        @inbounds for j = 1:2:L
             ns = zeros(4)   #left, top, right, bottom
             #bool? true:false
             i > 1 ? ns[1] = M[i-1, j] : ns[1] = M[L, j]
@@ -45,8 +45,8 @@ function get_E(M, J = 1)
             E_tot += sum(ns*M[i, j])
         end
     end
-    for i = 2:2:L
-        for j = 2:2:L
+    @inbounds for i = 2:2:L
+        @inbounds for j = 2:2:L
             ns = zeros(4)   #left, top, right, bottom
             #bool? true:false
             i > 1 ? ns[1] = M[i-1, j] : ns[1] = M[L, j]
@@ -101,13 +101,14 @@ function rand_ind(N, L)
 end
 
 function print_an_2x2(T, J = 1)
+    #prints analytical values for the 2x2 lattice
     Z = 4*(cosh(8J/T)+3)
     E = (32/Z)*sinh(-8J/T)
     Ma = (8/Z)*(exp(8J/T) + 2)
     Cv = (kb/(T^2))*((128/Z)*cosh(-8J/T) - E^2)
     chi = (32/(Z*T))*(exp(8J/T) + 1 - (2/Z)*(exp(16J/T) + 2*exp(8J/T) + 4))
     println("2x2 Analytical Data")
-    println("⟨ E ⟩, ⟨|M|⟩, Cᵥ, χ")
+    println("⟨E⟩, ⟨|M|⟩, Cᵥ, χ")
     println("$E, $Ma, $Cv, $chi")
 end
 
@@ -175,6 +176,7 @@ function drawLattice(M)
 end
 
 function plot_sim(M, L, t0, t1, N, J = 1)
+    #shows an image of the final lattice
     @time energy, magmom = sim(M, L, N, t0, t1, J)
     cycles = collect(0:N)
     plt.plot(cycles, energy)
@@ -188,6 +190,7 @@ function plot_sim(M, L, t0, t1, N, J = 1)
 end
 
 function sim(M, L, N, T, J = 1)
+    #simple simulation, doesnt return anything but the basic values
     Es = zeros(N+1)
     Es[1] = get_E(M, J)
     Ms = zeros(N+1)
@@ -203,6 +206,7 @@ end
 
 
 function step(M, t, ind, J = 1)
+    #one step of the algorithm
     L = length(M[1, :])
     final_ind = zeros(Int64, size(ind))
     ficount = 0
@@ -210,8 +214,7 @@ function step(M, t, ind, J = 1)
     for i=-8:4:8
         w[i+9] = exp(-J*i/t)
     end
-    #@sync @inbounds Threads.@threads for k = 1:size(ind)[1]
-    Threads.@threads for k = 1:size(ind)[1]
+    @inbounds for k = 1:size(ind)[1]
         i, j = ind[k, :]
         ns = zeros(4)   #left, top, right, bottom
         i > 1 ? ns[1] = M[i-1, j] : ns[1] = M[L, j]
@@ -229,19 +232,21 @@ function step(M, t, ind, J = 1)
 end
 
 function get_data(L, T, steps, J = 1)
+    #one run
     M = laticemaker(L, L)
     E = get_E(M)
     Ma = get_M(M)
     Etot = 0
-    EtotSq
+    Matot = 0
+    EtotSq = 0
     MatotSq = 0
     w = zeros(17)
     for i=-8:4:8
-        w[i+9] = exp(-J*i/t)
+        w[i+9] = exp(-J*i/T)
     end
     @inbounds for q = 1:steps
         ind = rand_ind(L^2, L)
-        @inbounds for k = 1:size(ind)[1]
+        Threads.@threads for k = 1:size(ind)[1]
             i, j = ind[k, :]
             ns = zeros(4)   #left, top, right, bottom
             i > 1 ? ns[1] = M[i-1, j] : ns[1] = M[L, j]
@@ -264,41 +269,51 @@ function get_data(L, T, steps, J = 1)
     Cv = EtotSq/steps - Etot^2/(steps^2)
     Chi = MatotSq/steps - Matot^2/(steps^2)
     return Etot/steps, Matot/steps, Cv, Chi
+end
 
 
-
-function get_values(L, T0, T1, steps, runs, J = 1, justones = false)
+function get_values(L, T0, T1, dt, steps, runs, J = 1)
     #=
-    Runs "sims" simulations and collects the different energy values with a corresponding
-    array of how often said values appear.
-    =#
-    Es = []
-    Mas = []
-    Cvs = []
-    Chis = []
-    dt = (T1-T0)/runs
-    Threads.@threads for i = 1:runs
-        E, Ma, Cv, Chi = get_data(L, T+i*dt)
-        append!(Es, E)
-        append!(Mas, Ma)
-        append!()
-        if i%percent == 0
-            current = i*100/runs
-            println("$i/$runs --> $current%")
-        end
+    Simulates, and finds average values for "runs" runs
+    =#   
+    Ts = T0:dt:T1
+    println("Initiating main simulation at: ", Dates.format(now(), "HH:MM:SS"))
+    println("Run info: $L x $L, T($T0-$T1), dt = $(round(dt; digits = 5))")
+    p = 10
+    fudgefactor = 1
+    t = @elapsed Threads.@threads for i = 1:p
+        get_data(L, Ts[1], steps, J)
     end
-    Es = sort!(Es)
-    return Es
+    totsteps = length(Ts)*runs
+    println("Estimated seconds until completion: $((t/p)*(totsteps*fudgefactor))")
+    Es = zeros(length(Ts))
+    Mas = zeros(length(Ts))
+    Cvs = zeros(length(Ts))
+    Chis = zeros(length(Ts))
+    @inbounds for i = 1:length(Ts)
+        vals = zeros(4)
+        Threads.@threads for q = 1:runs
+            vals = vals.+get_data(L, Ts[i], steps, J)
+        end
+        vals = vals./runs
+        Es[i] = vals[1]
+        Mas[i] = vals[2]
+        Cvs[i] = vals[3]
+        Chis[i] = vals[4]
+    end
+    println()
+    return Es, Mas, Cvs, Chis, Ts
 end
 
 function gifSim(savefile, M, L, T0, T1, N, J = 1)
+    #create a mp4 file of a simulation.
     skip = 1
     println("L $L steps $N t0 $T0 t1 $T1.mp4")
     fig = figure()
     im =  imshow(M, cmap = "binary")
     ims = [PyCall.PyObject[im]]
     dT = (T1-T0)/N
-    @time for i in 1:N-1
+    for i in 1:N-1
         ind = rand_ind(L^2, L)
         M = step(M, T0 + i*dT, ind, J)
         im = imshow(M, cmap = "binary")
@@ -310,6 +325,7 @@ function gifSim(savefile, M, L, T0, T1, N, J = 1)
 end
 
 function gifSims(Ls, T0s, T1s, stepss)
+    #run gifSim with several different combinaitons of variables
     for steps in stepss
         for T0 in T0s
             for T1 in T1s
@@ -323,63 +339,135 @@ function gifSims(Ls, T0s, T1s, stepss)
     end
 end
 
-function plot_values(Es, Ms, Cvs, Chis, Stables, Ts, L, steps)
+function plot_values(Es, Mas, Cvs, Chis, Ts, Ls, steps, runs, J = 1, analytical = false)
+    #=
+    Plots 2d arrays of the first 4 arguments against T. Saves as png.
+    If analytical = true, overlays analytical solution for 2x2
+    =#
     T0 = minimum(Ts)
     T1 = maximum(Ts)
 
-    plt.plot(T, Es)
+    Esa = zeros(length(Ts))
+    Msa = zeros(length(Ts))
+    Cvsa = zeros(length(Ts))
+    Chisa = zeros(length(Ts))
+
+    if analytical
+        for i = 1:length(Ts)
+            T = Ts[i]
+            Z = 4*(cosh(8J/T)+3)
+            Esa[i] = (128/Z)*(exp(8J/T) + exp(-8J/T))
+            Msa[i] = (32/Z)*(exp(8J/T) + 1)
+            Esaavg = -(16/Z)*(exp(8J/T) - exp(-8J/T))
+            Masaavg = (8/Z)*(exp(8J/T) + 2)
+            Cvsa[i] = (Esa[i] - Esaavg^2)/(T^2)
+            Chisa[i] = (Msa[i] - Masaavg^2)/(T^2)
+        end
+    end
+    println(size(Ts))
+    println(size(Es))
+    for i = 1:length(Es[:, 1])
+        plt.plot(Ts, Es[i, :], label = "L = $(Ls[i])")
+    end
     plt.xlabel("Temperature [kB*K]")
     plt.ylabel("Mean energy")
-    plt.title("$L x $L matrix with $steps steps per run")
+    plt.yscale("log")
+    plt.title("\$ \\langle E \\rangle \$")
+    plt.legend()
+    plt.savefig("E_$(Ls[1])_to_$(Ls[end])_$(steps)_T($(T0)_$(T1)).png")
+    plt.close()
 
-    plt.plot(T, Ms)
+    for i = 1:length(Mas[:, 1])
+        plt.plot(Ts, Mas[i, :], label = "L = $(Ls[i])")
+    end
     plt.xlabel("Temperature [kB*K]")
-    plt.ylabel("Mean absolute magnetisation")
-    plt.title("$L x $L matrix with $steps steps per run")
+    plt.ylabel("Mean energy")
+    plt.yscale("log")
+    plt.title("\$ \\langle |M| \\rangle \$")
+    plt.legend()
+    plt.savefig("Ma_$(Ls[1])_to_$(Ls[end])_$(steps)_T($(T0)_$(T1)).png")
+    plt.close()
 
-    plt.plot(T, Cvs)
+    for i = 1:length(Cvs[:, 1])
+        plt.plot(Ts, Cvs[i, :], label = "L = $(Ls[i])")
+    end
     plt.xlabel("Temperature [kB*K]")
-    plt.ylabel("Heat capacity")
-    plt.title("$L x $L matrix with $steps steps per run")
+    plt.ylabel("Mean energy")
+    plt.yscale("log")
+    plt.title("\$ C_V \$")
+    plt.legend()
+    plt.savefig("Cv_$(Ls[1])_to_$(Ls[end])_$(steps)_T($(T0)_$(T1)).png")
+    plt.close()
     
-    plt.plot(T, Chis)
+    for i = 1:length(Chis[:, 1])
+        plt.plot(Ts, Chis[i, :], label = "L = $(Ls[i])")
+    end
     plt.xlabel("Temperature [kB*K]")
-    plt.ylabel("Susceptebility")
-    plt.title("$L x $L matrix with $steps steps per run")
-    
-    plt.plot(T, Stables)
-    plt.xlabel("Temperature [kB*K]")
-    plt.ylabel("Runs before stability is achieved")
-    plt.title("$L x $L matrix with $steps steps per run")
-    
-function main()
-    test_all()
-    
-    L = 20                                  #Lattice Side Length
-    justones = false                       #make initial matrix all ones
-    M = laticemaker(L, L, justones)       #Lattice
-    #ind = rand_ind(L^2, L)                #random indexes
-    T0 = 1.4                               #Start Temperature
-    T1 = 1.4                               #Stop Temperature
-    J = 1                                  #J
-    steps = 1000                             #Monte-Carlo Cycles per run
-    runs = 1000                           #sample size
+    plt.ylabel("Mean energy")
+    plt.yscale("log")
+    plt.title("\$ \\chi \$")
+    plt.legend()
+    plt.savefig("Chi_$(Ls[1])_to_$(Ls[end])_$(steps)_T($(T0)_$(T1)).png")
+    plt.close()
+end
 
-    @time Es, Ms, Cvs, Chis, steps, M = sim(M, L, steps, T, J)
-    #drawLattice(data[3])
-    #plot_sim(M, L, T0, T1, steps, J)
-    #@time Edist = probEcounter(L, steps, T0, T1, runs, J, justones)
-    plt.plot(collect(1:length(Es)), Es)
-    plt.show()
-    Ls = [1000]
-    T0s = [2.26]            #Start Temperature
-    T1s = [2.26]
-    stepss = [100]
-    #gifSims(Ls, T0s, T1s, stepss)
-    #print_an_2x2(T0)
+function print_values(Es, Mas, T, steps)
+    #prints analytical values 
+    Z = 4*(cosh(8J/T)+3)
+    Esa= (128/Z)*(exp(8J/T) + exp(-8J/T))
+    Msa= (32/Z)*(exp(8J/T) + 1)
+    Esaavg = -(16/Z)*(exp(8J/T) - exp(-8J/T))
+    Masaavg = (8/Z)*(exp(8J/T) + 2)
+    Cvsa= (Esa - Esaavg^2)/(T^2)
+    Chisa = (Msa - Masaavg^2)/(T^2)
+    
 end
 
 
-main()
+function main()
+    test_all()
+    Ls = [20, 40, 60, 80, 100]
+    steps = 800
+    runs = 5
+    J = 1
+    lenTs = 30
+    Es = zeros((length(Ls), lenTs+1))
+    Mas = zeros((length(Ls), lenTs+1))
+    Cvs = zeros((length(Ls), lenTs+1))
+    Chis = zeros((length(Ls), lenTs+1))
+    Ts = 0
+    T0 = 1
+    print_an_2x2(T0)
+    println(get_values(2, 1, 2, 1, 1000, 10000, J))
+    println()
+    for i = 1:length(Ls)
+        L = Ls[i]
+        T0 = 1
+        T1 = 3
+        dt = (T1-T0)/lenTs
+        Es[i, :], Mas[i, :], Cvs[i, :], Chis[i, :], Ts = get_values(L, T0, T1, dt, steps, runs, J)
+    end
+    plot_values(Es, Mas, Cvs, Chis, Ts, Ls, steps, runs, J)
 
-#bytte ut rand_ind med en rand_mat?
+    """
+    @inbounds for L in Ls
+        T0 = 1
+        T1 = 3
+        dt = (T1-T0)/100
+        Es, Ms, Cvs, Chis, Ts = get_values(L, T0, T1, dt, steps, runs, J)
+        plot_values(Es, Ms, Cvs, Chis, Ts, L, steps, runs, J)
+        
+        T0 = 2
+        T1 = 2.3
+        dt = (T1-T0)/100
+        Es, Ms, Cvs, Chis, Ts = get_values(L, T0, T1, dt, steps, runs, J)
+        plot_values(Es, Ms, Cvs, Chis, Ts, L, steps, runs, J, anal)
+    end
+    """
+    #T0 = 1
+    #T1 = 1
+    #gifSims(Ls, T0s, T1s, stepss)
+    #Es, Ms, Cvs, Chis, Ts = get_values(L, T0, T1, dt, steps, runs, J)
+end
+
+main()
