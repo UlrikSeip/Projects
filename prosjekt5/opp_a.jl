4ee814994337d23eb39797c302e9b27405b0b0c6
bendkok <bendiksd@student.matnat.uio.no>
2018-12-11 17:10:49 +0100

    la til tex-fil

--- prosjekt5/opp_a.jl
+++ prosjekt5/opp_a.jl
@@ -48,7 +48,7 @@ function main(a,b,c,S0,I0,R0,T,dt,filename)
     plt.legend(["Susceptible", "Infected", "Recovered"])
     plt.xlabel("Days")
     plt.ylabel("People")
-    plt.savefig("C:\\Users\\Bendik\\Documents\\GitHub\\Projects\\prosjekt5\\plots/"*filename)
+    #plt.savefig("C:\\Users\\Bendik\\Documents\\GitHub\\Projects\\prosjekt5\\plots/"*filename)
     #plt.savefig("/plots/"*filename)
     plt.show()
 end
@@ -57,8 +57,8 @@ S0 = 300
 I0 = 100
 R0 = 0
 
-#main(4,1,0.5, 300,100,0, 13,.01, "opp_a_A.pdf")
+main(4,1,0.5, 300,100,0, 13,.01, "opp_a_A.pdf")
 #main(4,2,0.5, 300,100,0, 21,.01, "opp_a_B.pdf")
 #main(4,3,0.5, 300,100,0, 27,.01, "opp_a_C.pdf")
 #main(4,4,0.5, 300,100,0, 27,.01, "opp_a_D.pdf")
-main(4,5,0.5, 300,100,0, 27,.01, "opp_a_E.pdf")
+#main(4,5,0.5, 300,100,0, 27,.01, "opp_a_E.pdf")
